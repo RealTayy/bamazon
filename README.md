120a95ddaba73976de0ac041cce00e6ceaff6db0
The Mai <Mai.The.09@gmail.com>
2018-04-24 02:18:29 -0500

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # bamazon
+bamazon is a CLI that simulates an online store. It contains 3 programs which interact with the same database:
+* Customer
+ ** Can buy products from the store
+* Manager
+ ** View Products for Sale
+ ** View Low Inventory (Item less then 20)
+ ** Add to Inventory
+ ** Add New Product
+* Supervisor
+ ** Can buy products from the store
+ ** Can buy products from the store
+
+##
---
+++ bamazonSeed.sql
@@ -0,0 +1,40 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("PS4", "Electronics", 399.99, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Xbox One", "Electronics", 499.99, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("70-INCH LED 4K HD TV", "Electronics", 1099.99, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Google home mini", "Electronics", 49.99, 150);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("King size bed", "Furniture", 559.99, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Love sofa", "Furniture", 399.99, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Wooden Bookcase", "Furniture", 99.99, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Bear rug", "Furniture", 129.99, 15);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Oxford shoes", "Clothing", 39.99, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Plain White Tee", "Clothing", 9.99, 500);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Compression pants", "Clothing", 19.99, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Under Armor Socks", "Clothing", 4.99, 1000);
+
+INSERT INTO departments (department_name, over_head_costs) VALUES
+("Electronics", 10000.00),
+("Furniture", 7500.00),
+("Clothing", 4000.00);
